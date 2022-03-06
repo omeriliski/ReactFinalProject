dc8682c6067e6d5faf5461fe67a0e4560f3ba08a
slawomir.krzyzak@gmail.com <slawomir.krzyzak@gmail.com>
2022-03-06 20:54:13 +0100

    J

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,13 +5,15 @@ import Logo from "./posts/img/logo.svg"
 export function Header(){
     return (
       <div className="header-wrapper">
-          <div className='logo'>
-              <img src={Logo}/>
-               <img src={Plus}/></div>
-          <div className='plus-for-post'></div>
-          <div className='hamburger-menu'></div>
-        <button className='logout-button' >Logout</button>
-      
+        <div className="logo">
+          <img src={Logo} />
+        </div>
+  
+        <div className="plus-for-post">
+        <img src={Plus} />
+          </div>
+        <div className="hamburger-menu"></div>
+        <button className="logout-button">Logout</button>
       </div>
     );
 }
