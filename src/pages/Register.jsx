80fe1bec259e63808462c4bcb96a07aa84922124
slawomir.krzyzak@gmail.com <slawomir.krzyzak@gmail.com>
2022-03-09 08:43:46 +0100

    Registration CSS

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,50 +1,58 @@
-import {useRef} from "react";
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
-import {useNavigate, NavLink} from "react-router-dom";
+import { useRef } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useNavigate, NavLink } from "react-router-dom";
+import "./register.scss";
+import { ButtonPrimary, ButtonSecondary } from "../posts/Buttons";
 
-const Register=()=>{
-    const username = useRef();
-    const email = useRef();
-    const password1 = useRef();
-    const password2 = useRef();
-    const auth = getAuth();
-    const navigate = useNavigate();
+const Register = () => {
+  const username = useRef();
+  const email = useRef();
+  const password1 = useRef();
+  const password2 = useRef();
+  const auth = getAuth();
+  const navigate = useNavigate();
 
-    const eventHandler=(e)=>{
-        e.preventDefault();
-        createUserWithEmailAndPassword(auth, email.current.value, password1.current.value)
-        .then(()=>{
-            console.log('registered :>> ');
-            //   router.push('/')
-            navigate("/login");
-        })
-        .catch (e=>{
-          alert(e.message)
-      })
-    }
-    return(
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={eventHandler}>
-                <div>
-                    <input type="text" ref={username}/>
-                </div>
-                <div>
-                    <input type="email" ref={email}/>
-                </div>
-                <div>
-                    <input type="password" ref={password1} />
-                </div>
-                <div>
-                    <input type="password" ref={password2} />
-                </div>
-                <div>
-                    <input type="submit" value="Registrieren"/>
-                </div>
-                <NavLink to="/login">Anmelden</NavLink>
-            </form>
-        </div>
+  const eventHandler = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      email.current.value,
+      password1.current.value
     )
-}
+      .then(() => {
+        console.log("registered :>> ");
+        //   router.push('/')
+        navigate("/login");
+      })
+      .catch((e) => {
+        alert(e.message);
+      });
+  };
+  return (
+    <div className="card-div">
+      <h1>Register</h1>
+      <form onSubmit={eventHandler}>
+        <div>
+          <input type="text" ref={username} placeholder="Username"/>
+        </div>
+        <div>
+          <input type="email" ref={email} placeholder="E-Mail"/>
+        </div>
+        <div>
+          <input type="password" ref={password1} placeholder="Password"/>
+        </div>
+        <div>
+          <input type="password" ref={password2} placeholder="Repeat the password"/>
+        </div>
+        <div>
+          <ButtonPrimary />
+        </div>
+        <NavLink to="/login">
+          <ButtonSecondary />
+        </NavLink>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
