433df3f70b7b798501bc074d3e8023b270d40768
slawomir.krzyzak@gmail.com <slawomir.krzyzak@gmail.com>
2022-03-18 10:56:50 +0100

    survey answer yellow bg

--- src/components/SurveyResult.jsx
+++ src/components/SurveyResult.jsx
@@ -1,24 +1,26 @@
 const SurveyResult=({post})=>{
     
-    return(
-        <div>
-            {/* {post.answer.map(item=>console.log('item :>> ', item))}
+    return (
+      <div className="interactions-overlay w-100 p-16">
+        {/* {post.answer.map(item=>console.log('item :>> ', item))}
             {post.answer.filter(item=>item.answer && currentUser.email)} */}
-            
-            
-            {post.options.map(element =>
-                <div>
-                    {element}:{post.answer.filter(item=>item.answer==element).length}
-                </div>
+        <div className="txt-title">Feedbacks</div>
+        {post.options.map(
+          (element) => (
+            <div className="answer-wrapper">
+              <div className="answer-bar" style={{minWidth:`${post.answer.filter((item) => item.answer == element).length/post.answer.length*90}%`,  backgroundColor: "yellow"} }>
+                {element}:
+                {post.answer.filter((item) => item.answer == element).length}
+              </div>
+            </div>
+          )
 
-                
-                // post.answer.forEach(item=>console.log('item :>> ', item.answer))
-                // console.log('element :>> ', element);
-                // console.log('post ', post.answer.filter(item=>item.answer==element).length) 
-                
-            )}
-            {post.answer.length} people answered
-        </div>
-    )
+          // post.answer.forEach(item=>console.log('item :>> ', item.answer))
+          // console.log('element :>> ', element);
+          // console.log('post ', post.answer.filter(item=>item.answer==element).length)
+        )}
+        {post.answer.length} people answered
+      </div>
+    );
 }
 export default SurveyResult;
